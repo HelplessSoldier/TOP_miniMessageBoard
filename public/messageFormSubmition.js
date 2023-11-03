cd120240b440185cc3df9305a83930e0a1469104
HelplessSoldier <patrick1234143211@gmail.com>
2023-11-03 14:23:47 -0700

    basic empty form vali

--- public/messageFormSubmition.js
+++ public/messageFormSubmition.js
@@ -1,9 +1,19 @@
-const warning = document.getElementById('formEmptyWarning');
-document.getElementById('messageForm').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const formData = new FormData(e.target);
-  if (formData.get("user-name") !== "" && formData.get("message") !== "") {
-    console.log('hi c:');
-    return;
-  }
+document.addEventListener('DOMContentLoaded', (e) => {
+  const messageForm = document.getElementById('messageForm')
+  const warning = document.getElementById('formEmptyWarning');
+
+  messageForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    if (
+      formData.get("user-name") !== "" &&
+      formData.get("message") !== ""
+    ) {
+      console.log('hi c:');
+      return;
+    } else {
+      warning.style.visibility = 'visible';
+    }
+  })
 })
+
